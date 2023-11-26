ae6d166aa652b92a4f49420ddc2154f517ded174
Gökçe Usta <gokce1usta@gmail.com>
2023-11-26 14:39:23 +0300

    DoktorBilgi

---
+++ HastaneSistemi/HastaneSistemi/Models/Doktor.cs
@@ -0,0 +1,13 @@
+﻿namespace HastaneSistemi.Models
+{
+    public class Doktor
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public string Department { get; set; }
+
+
+    }
+}
