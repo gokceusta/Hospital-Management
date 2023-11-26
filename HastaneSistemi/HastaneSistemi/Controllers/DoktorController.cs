2026d8314dd217aa5039972db38b3339021549c2
Gökçe Usta <gokce1usta@gmail.com>
2023-11-26 03:17:56 +0300

    Doktor alanı düzenleme
    Doktor ve Login Düzenleme

---
+++ HastaneSistemi/HastaneSistemi/Controllers/DoktorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HastaneSistemi.Controllers
+{
+    public class DoktorController : Controller
+    {
+        public IActionResult DoktorBilgi()
+        {
+            return View();
+        }
+    }
+}
